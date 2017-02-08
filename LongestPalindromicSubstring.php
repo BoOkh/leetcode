ba3b39d76ddd703b0fef2bff9d872385d42ab0f6
BoOkh <bohdan.okhrimchuk@gmail.com>
2017-02-08 14:21:44 +0200

    Imlemented task - Longest Palindromic Substring

---
+++ LongestPalindromicSubstring.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @link https://leetcode.com/problems/longest-palindromic-substring/
+ * Class LongestPalindromicSubstring
+ */
+class LongestPalindromicSubstring
+{
+    public static function calculate($string = '')
+    {
+        $tempString = '';
+        $givenValues = array();
+
+        $offset = 0;
+        $length = strlen($string);
+
+        while ($offset < $length) {
+            for ($i = $offset; $i < $length; $i++) {
+                $tempString .= $string[$i];
+
+                if (strrev($tempString) == $tempString) {
+                    $givenValues[] = $tempString;
+                }
+            }
+
+            $tempString = '';
+            $offset++;
+        }
+
+        $lengths = array_map('strlen', $givenValues);
+        $maxLength = max($lengths);
+        $arrayIndex = array_search($maxLength, $lengths);
+
+        return $givenValues[$arrayIndex];
+    }
+}
+
+$string = 'babad';
+
+$result = LongestPalindromicSubstring::calculate($string);
+
+echo $result;
